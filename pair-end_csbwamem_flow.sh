b28f9e4f728022e6a0854b8a2b225b8e6f913b0a
Max Grossman <jmaxg3@gmail.com>
2015-06-15 09:07:52 -0500

    working test runs

--- pair-end_csbwamem_flow.sh
+++ pair-end_csbwamem_flow.sh
@@ -19,32 +19,46 @@ if [[ $TASK != upload && $TASK != kernel ]]; then
     exit 1
 fi
 
+N_NODES=$(cat ${HADOOP_HOME}/etc/hadoop/slaves | wc -l)
+CORES_PER_NODE=$(cat /proc/cpuinfo | grep processor | wc -l)
+TOTAL_CORES=$(echo $N_NODES \* $CORES_PER_NODE | bc)
+
 if [[ $TASK == upload ]]; then
     # store RDD
     # pair-end
-    SPARK_DRIVER_MEMORY=48g $SPARK_HOME/bin/spark-submit --executor-memory 40g \
-        --class cs.ucla.edu.bwaspark.BWAMEMSpark --total-executor-cores 20 \
+    echo "Starting upload"
+    SPARK_DRIVER_MEMORY=40g $SPARK_HOME/bin/spark-submit --executor-memory 40g \
+        --class cs.ucla.edu.bwaspark.BWAMEMSpark --total-executor-cores $TOTAL_CORES \
         --master spark://$(hostname):7077 \
         --driver-java-options "-XX:+PrintFlagsFinal" \
+        --conf spark.akka.frameSize=30 \
         ${BWAMEM_HOME}/target/cloud-scale-bwamem-0.1.0-assembly.jar upload-fastq \
-        -bn 10000000 1 280 /scratch/jmg3/HCC1954_1_1m.fq \
-        /scratch/jmg3/HCC1954_2_1m.fq hdfs://$(hostname):54310/HCC1954_1mreads.fq
+        -bn 1000000 1 $TOTAL_CORES /scratch/jmg3/HCC1954_1_10M.fq \
+        /scratch/jmg3/HCC1954_2_10M.fq hdfs://$(hostname):54310/HCC1954_10Mreads.fq
+    echo "Done with upload"
 fi
 
 if [[ $TASK == kernel ]]; then
     # run cloud-scale bwamem
     # SAM output
+    # When running with 10M reads, the last parameter (which indicates the
+    # number of input files to read) must be set to 20. When doing 100M reads, I
+    # believe it is supposed to be set to 80?
+    echo "Starting kernel"
     SPARK_DRIVER_MEMORY=40g $SPARK_HOME/bin/spark-submit --executor-memory 40g \
-        --class cs.ucla.edu.bwaspark.BWAMEMSpark --total-executor-cores 20 \
+        --class cs.ucla.edu.bwaspark.BWAMEMSpark --total-executor-cores $TOTAL_CORES \
         --master spark://$(hostname):7077 \
         --driver-java-options "-XX:+PrintFlagsFinal" \
-        --conf spark.driver.maxResultSize=60g \
+        --conf spark.driver.maxResultSize=40g \
         ${BWAMEM_HOME}/target/cloud-scale-bwamem-0.1.0-assembly.jar cs-bwamem \
-        -bfn 0 -bPSW 1 -sbatch 10 -bPSWJNI 1 \
+        -bfn 20 -bPSW 1 -sbatch 5 -bPSWJNI 1 \
         -jniPath ${BWAMEM_HOME}/target/jniNative.so -oChoice 1 \
-        -oPath /scratch/jmg3/HCC1954_1mreads.sam 1 \
+        -oPath /scratch/jmg3/HCC1954_10Mreads.sam \
+        1 \
         /scratch/jmg3/ReferenceMetadata/human_g1k_v37.fasta \
-        hdfs://$(hostname):54310/HCC1954_1mreads.fq 80
+        hdfs://$(hostname):54310/HCC1954_10Mreads.fq \
+        20
+    echo "Done with kernel"
 fi
 
 # ADAM output
